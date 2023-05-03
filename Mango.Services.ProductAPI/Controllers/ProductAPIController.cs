c821f4b99b742987176affcf4be7956d43f6bcb3
Evan Malherbe <evan@ix.co.za>
2023-05-03 07:45:34 +0200

    Added remaining Crud operation routes to controller.

--- Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -35,7 +35,6 @@ namespace Mango.Services.ProductAPI.Controllers
 			return _response;
 		}
 
-		
 		[HttpGet]
 		[Route("{id}")]
 		public async Task<object> Get(int id)
@@ -44,6 +43,64 @@ namespace Mango.Services.ProductAPI.Controllers
 			{
 				ProductDTO productDTO = await _productRepository.GetProductById(id);
 				_response.Result = productDTO;
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.ErrorMessages = new List<string>()
+				{
+					ex.ToString()
+				};
+			}
+			return _response;
+		}
+
+		[HttpPost]
+		public async Task<object> Post([FromBody] ProductDTO productDTO)
+		{
+			try
+			{
+				ProductDTO model = await _productRepository.CreateUpdateProduct(productDTO);
+				_response.Result = model;
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.ErrorMessages = new List<string>()
+				{
+					ex.ToString()
+				};
+			}
+			return _response;
+		}
+
+		[HttpPut]
+		public async Task<object> Put([FromBody] ProductDTO productDTO)
+		{
+			try
+			{
+				ProductDTO model = await _productRepository.CreateUpdateProduct(productDTO);
+				_response.Result = model;
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.ErrorMessages = new List<string>()
+				{
+					ex.ToString()
+				};
+			}
+			return _response;
+		}
+
+		
+		[HttpDelete]
+		public async Task<object> Delete(int id)
+		{
+			try
+			{
+				bool isSuccess = await _productRepository.DeleteProduct(id);
+				_response.Result = isSuccess;
 			}
 			catch (Exception ex)
 			{
