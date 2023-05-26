8ea79b93b898ebf55a20b09abe22c3a5b767eb45
Evan Malherbe <evan@ix.co.za>
2023-05-26 09:20:38 +0200

    Seed database

---
+++ Mango.Services.Identity/Initialiser/IDbInitialiser.cs
@@ -0,0 +1,7 @@
+﻿namespace Mango.Services.Identity.Initialiser
+{
+  public interface IDbInitialiser
+  {
+    public void Initialise();
+  }
+}
--- Mango.Services.Identity/Program.cs
+++ Mango.Services.Identity/Program.cs
@@ -1,4 +1,34 @@
+using Mango.Services.Identity;
+using Mango.Services.Identity.DbContexts;
+using Mango.Services.Identity.Initialiser;
+using Mango.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(connectionString));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+  .AddEntityFrameworkStores<ApplicationDbContext>()
+  .AddDefaultTokenProviders();
+
+var buildI = builder.Services.AddIdentityServer(options =>
+{
+  options.Events.RaiseErrorEvents = true;
+  options.Events.RaiseInformationEvents = true;
+  options.Events.RaiseFailureEvents = true;
+  options.Events.RaiseSuccessEvents = true;
+  options.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(SD.IdentityResources)
+.AddInMemoryApiScopes(SD.ApiScopes)
+.AddInMemoryClients(SD.Clients)
+.AddAspNetIdentity<ApplicationUser>();
+
+builder.Services.AddScoped<IDbInitialiser, DbInitialiser>();
+buildI.AddDeveloperSigningCredential();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,8 +47,20 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseIdentityServer();
 
 app.UseAuthorization();
+SeedDatabase();
+//dbInitialiser.Initialise();
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+      var dbInitialiser = scope.ServiceProvider.GetRequiredService<IDbInitialiser>();
+      dbInitialiser.Initialise();
+    }
+}
 
 app.MapControllerRoute(
     name: "default",
