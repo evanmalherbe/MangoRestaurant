88757f088246a780df893776c4bcddb7272d4b04
Evan Malherbe <evan@ix.co.za>
2023-05-31 09:48:12 +0200

    Finally fixed the incorrect uri so that the login page appears without errors.

--- Mango.Services.Identity/Program.cs
+++ Mango.Services.Identity/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(connectionString));
+  options.UseSqlServer(connectionString));
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
   .AddEntityFrameworkStores<ApplicationDbContext>()
--- Mango.Services.Identity/SD.cs
+++ Mango.Services.Identity/SD.cs
@@ -40,8 +40,8 @@ namespace Mango.Services.Identity
           ClientId="mango",
           ClientSecrets= {new Secret("secret".Sha256())},
           AllowedGrantTypes = GrantTypes.Code, 
-          RedirectUris = {"https://localhost:44374/signin-oidc"},
-          PostLogoutRedirectUris={"https://localhost:44374/signout-callback-oidc"},
+          RedirectUris = {"https://localhost:7217/signin-oidc"},
+          PostLogoutRedirectUris={"https://localhost:7217/signout-callback-oidc"},
           AllowedScopes = new List<string>
           {
             IdentityServerConstants.StandardScopes.OpenId,
