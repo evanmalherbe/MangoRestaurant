71456a2b2acf16f3df7a3efbd83a9023d0b2daa5
Evan Malherbe <evan@ix.co.za>
2023-10-21 09:23:16 +0200

    Added Coupon API service

---
+++ Mango.Services.CouponAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+
+namespace Mango.Services.CouponAPI
+{
+	public class MappingConfig
+	{
+		public static MapperConfiguration RegisterMaps()
+		{
+			var mappingConfig = new MapperConfiguration(config =>
+			{
+				//config.CreateMap<ProductDTO, Product>().ReverseMap();
+				//config.CreateMap<CartHeader, CartHeaderDTO>().ReverseMap();
+				//config.CreateMap<CartDetails, CartDetailsDTO>().ReverseMap();
+				//config.CreateMap<Cart, CartDTO>().ReverseMap();
+			});
+
+			return mappingConfig;
+		}
+	}
+}
